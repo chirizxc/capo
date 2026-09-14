6b15bfaea06cb8ddd80f0d755fdf33526be189f2
karpetrosyan <kar.petrosyanpy@gmail.com>
2026-09-15 01:36:36 +0400

    fix lex-model-building services

--- services/lex-model-building-service/src/capo_lex_model_building_service/errors/resource_in_use_exception.py
+++ services/lex-model-building-service/src/capo_lex_model_building_service/errors/resource_in_use_exception.py
@@ -74,7 +74,7 @@ class ResourceInUseException(ServiceError):
             is_throttling_error=False,
             is_retryable=False,
             code="ResourceInUseException",
-            message=message if message is not None else data.get("message"),
+            message=message,
         )
         self.data = data
 
